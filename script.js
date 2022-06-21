a79cd613fed7d51d4b0baf60a31f55b409ce8ab1
Rosechelle Dianzon <93816271+kaedeciel@users.noreply.github.com>
2022-06-21 23:06:16 +0800

    color change
    card colors

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const poke_container = document.getElementById('poke-container')
 const pokemon_count = 150
 const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    fire: '#EE8130',
+    grass: '#7AC74C',
+	electric: '#F7D02C',
+	water: '#6390F0',
+	ground: '#E2BF65',
+	rock: '#B6A136',
+	fairy: '#D685AD',
+	poison: '#A33EA1',
+	bug: '#A6B91A',
+	dragon: '#6F35FC',
+	psychic: '#F95587',
+	flying: '#A98FF3',
+	fighting: '#C22E28',
+	normal: '#A8A77A'
 }
 
 const main_types = Object.keys(colors)
