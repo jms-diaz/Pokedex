dc96631a3b418a118f3d52f7326ea3247b6cc9c2
John Martin De Guia <jmartindeguia09@gmail.com>
2022-06-21 14:01:17 +0800

    search pokemon page styling changed

--- searchPokemon.js
+++ searchPokemon.js
@@ -8,6 +8,23 @@ const pokemonType = document.querySelector("#pokemon-type");
 const pokemonWeight = document.querySelector("#weight");
 const pokemonHeight = document.querySelector("#height");
 
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
 function searchPokemon() {
   form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -21,8 +38,57 @@ function searchPokemon() {
         const pWeight = data.weight;
         const pTypes = data.types[0].type.name;
 
+        switch (pTypes) {
+          case "fire":
+            pokemonCard.style.background = colors.fire;
+            break;
+          case "grass":
+            pokemonCard.style.background = colors.grass;
+            break;
+          case "electric":
+            pokemonCard.style.background = colors.electric;
+            break;
+          case "water":
+            pokemonCard.style.background = colors.water;
+            break;
+          case "ground":
+            pokemonCard.style.background = colors.ground;
+            break;
+          case "rock":
+            pokemonCard.style.background = colors.rock;
+            break;
+          case "fairy":
+            pokemonCard.style.background = colors.fairy;
+            break;
+          case "poison":
+            pokemonCard.style.background = colors.poison;
+            break;
+          case "bug":
+            pokemonCard.style.background = colors.bug;
+            break;
+          case "dragon":
+            pokemonCard.style.background = colors.dragon;
+            break;
+          case "psychic":
+            pokemonCard.style.background = colors.psychic;
+            break;
+          case "flying":
+            pokemonCard.style.background = colors.flying;
+            break;
+          case "fighting":
+            pokemonCard.style.background = colors.fighting;
+            break;
+          case "normal":
+            pokemonCard.style.background = colors.normal;
+            break;
+          default:
+            pokemonCard.style.background = "#fff";
+        }
+
+        pokemonCard.style.display = "block";
+
         pokemonName.textContent = pName;
-        pokemonImg.innerHTML = `<img src=${pImage} alt=${pName} />`;
+        pokemonImg.innerHTML = `<img class="pokemon" src=${pImage} alt=${pName} />`;
         pokemonWeight.textContent = "Weight: " + pWeight / 10 + "lbs";
         pokemonHeight.textContent = "Height: " + pHeight / 10 + "m";
         pokemonType.textContent = "Type: " + pTypes;
@@ -34,8 +100,6 @@ function searchPokemon() {
         searchInput.value = "";
         pokemonCard.style.display = "none";
       });
-
-    pokemonCard.style.display = "block";
   });
 }
 
