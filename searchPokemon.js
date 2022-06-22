5e23f1c03d4635eec0025c16d4fca3cb66596411
John Martin De Guia <jmartindeguia09@gmail.com>
2022-06-22 09:40:46 +0800

    search pokemon card style changed

--- searchPokemon.js
+++ searchPokemon.js
@@ -9,20 +9,20 @@ const pokemonWeight = document.querySelector("#weight");
 const pokemonHeight = document.querySelector("#height");
 
 const colors = {
-  fire: "#FDDFDF",
-  grass: "#DEFDE0",
-  electric: "#FCF7DE",
-  water: "#DEF3FD",
-  ground: "#f4e7da",
-  rock: "#d5d5d4",
-  fairy: "#fceaff",
-  poison: "#98d7a5",
-  bug: "#f8d5a3",
-  dragon: "#97b3e6",
-  psychic: "#eaeda1",
-  flying: "#F5F5F5",
-  fighting: "#E6E0D4",
-  normal: "#F5F5F5",
+  fire: "#EE8130",
+  grass: "#7AC74C",
+  electric: "#F7D02C",
+  water: "#6390F0",
+  ground: "#E2BF65",
+  rock: "#B6A136",
+  fairy: "#D685AD",
+  poison: "#A33EA1",
+  bug: "#A6B91A",
+  dragon: "#6F35FC",
+  psychic: "#F95587",
+  flying: "#A98FF3",
+  fighting: "#C22E28",
+  normal: "#A8A77A",
 };
 
 function searchPokemon() {
